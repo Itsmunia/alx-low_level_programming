6c029e4d9e49f99832ec5af15a5e73a5da4cbf16
Munia <Munia@gmail.com>
2023-09-07 05:52:56 -0700

    fgh

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,13 +7,10 @@
  * Description: print a quote using write function
  * write (int fd.const void "buf.size_t count")
  * Return: 1 (not success)
- 
-# Please enter the commit message for your changes. Lines starting
  */
 
 int main(void)
 {
-	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-		write(2.quo.59);
+		write(2 , "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" , 59);
 	return (1);
 }
